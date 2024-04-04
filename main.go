a662e56faa54682139db3af1f09ca9b97e258736
Bickor <martin.heberling.dalies@gmail.com>
2024-04-03 19:59:40 -0700

    Made client/server.

--- main.go
+++ main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"os"
 )
 
 const (
 	height = 6
 	width  = 7
+
+	SERVER_HOST = "localhost"
+	SERVER_PORT = "8080"
+	SERVER_TYPE = "tcp"
 )
 
 func main() {
@@ -25,6 +31,63 @@ func main() {
 		grid[i] = make([]uint8, width)
 	}
 	fmt.Println(grid)
+
+	var first string
+	fmt.Scanln(&first)
+	if first == "1" {
+		startServer()
+	} else if first == "2" {
+		connectClient()
+	}
+}
+
+func startServer() {
+	fmt.Println("Server Running...")
+	server, err := net.Listen(SERVER_TYPE, SERVER_HOST+":"+SERVER_PORT)
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	defer server.Close()
+	fmt.Println("Listening on " + SERVER_HOST + ":" + SERVER_PORT)
+	fmt.Println("Waiting for client...")
+	for {
+		connection, err := server.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		fmt.Println("client connected")
+		go processClient(connection)
+	}
+}
+
+func processClient(connection net.Conn) {
+	buffer := make([]byte, 1024)
+	mLen, err := connection.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+	}
+	fmt.Println("Received: ", string(buffer[:mLen]))
+	_, err = connection.Write([]byte("Thanks! Got your message:" + string(buffer[:mLen])))
+	connection.Close()
+}
+
+func connectClient() {
+	//establish connection
+	connection, err := net.Dial(SERVER_TYPE, SERVER_HOST+":"+SERVER_PORT)
+	if err != nil {
+		panic(err)
+	}
+	///send some data
+	_, err = connection.Write([]byte("Hello Server! Greetings."))
+	buffer := make([]byte, 1024)
+	mLen, err := connection.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+	}
+	fmt.Println("Received: ", string(buffer[:mLen]))
+	defer connection.Close()
 }
 
 // Grid is 6 high by 7 wide. Alternating colors.
